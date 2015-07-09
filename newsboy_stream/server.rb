e8cf18511ddceba4239d4564c92405e6f8875681
Sam Kreter <samkreter@gmail.com>
2015-07-09 17:57:00 -0400

    added stories json api

--- newsboy_stream/server.rb
+++ newsboy_stream/server.rb
@@ -9,6 +9,11 @@ configure do
   set :database, {adapter: "sqlite3", database: "news.sqlite3"}
 end
 
+get '/api/stories' do
+  content_type :json
+  {stories: Story.all}.to_json
+end
+
 get '*' do
   send_file 'dist/index.html'
 end
