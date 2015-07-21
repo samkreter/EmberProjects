1c2d34d688fe1b668d2d65adc40ecdcc6917660b
Sam Kreter <samkreter@gmail.com>
2015-07-20 22:27:07 -0400

    added details display for one company

--- business-visualiation/app/router.js
+++ business-visualiation/app/router.js
@@ -2,14 +2,14 @@ import Ember from 'ember';
 import config from './config/environment';
 
 var Router = Ember.Router.extend({
-  location: config.locationType
+    location: config.locationType
 });
 
 Router.map(function() {
-  this.resource('companies',{path: '/' },function(){
-    this.route('company',{ path: '/:company_id'});
-  });
-
+    this.resource('companies', { path: '/' }, function() {
+      this.resource('company', { path: '/:company_id' });
+    });
 });
+
 
 export default Router;
